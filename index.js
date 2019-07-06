cb71ed80c066cab4e1e63da09f572bb1c1237b54
Will Gutierrez <will.gutierrez@gmail.com>
2019-07-06 11:30:21 -0700

    swaps out axios for puppeteer

--- index.js
+++ index.js
@@ -1,8 +1,42 @@
 require('dotenv').config()
-const axios = require('axios')
-const cheerio = require('cheerio')
-const fs = require('fs')
-const async = require('async')
+// const axios = require('axios')
+// const cheerio = require('cheerio')
+// const fs = require('fs')
+// const async = require('async')
+
+const puppeteer = require('puppeteer')
+
+async function loginToAdminAccount() {
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
+  await page.goto('https://admin.threestonehearth.com/login')
+  await page.type('#Email', process.env['THREESTONE_HEARTH_ADMIN_EMAIL'])
+  await page.type('#Password', process.env['THREESTONE_HEARTH_ADMIN_PASSWORD'])
+
+  await page.screenshot({ path: './screenshots/screenshot.png'  })
+
+  browser.close()
+
+  // await page.click('[class="button-1 login-button"]')
+  // await page.waitForNavigation()
+  // await page.screenshot({ path: 'screenshots' })
+  // browser.close()
+}
+
+loginToAdminAccount()
+
+// (async () => {
+//   const browser = await puppeteer.launch()
+//   const page = await browser.newPage()
+//   await page.goto('https://admin.threestonehearth.com/login')
+//   await page.type('#Email', process.env['THREESTONE_HEARTH_ADMIN_EMAIL'])
+//   await page.type('#Password', process.env['THREESTONE_HEARTH_ADMIN_PASSWORD'])
+//   await page.click('[class="login-button"]')
+//   await page.waitForNavigation()
+//   await page.screenshot({ path: './screenshots' })
+//   browser.close()
+// })();
+
 
 // axios.post('https://admin.threestonehearth.com/login', {
 //   Email: process.env['THREESTONE_HEARTH_ADMIN_EMAIL'],
@@ -19,39 +53,39 @@ const async = require('async')
 //   	console.log('Error logging in: ' + error)
 //   })
 
-async.series([
-  (callback) => {
-    axios.post('https://admin.threestonehearth.com/login', {
-      Email: process.env['THREESTONE_HEARTH_ADMIN_EMAIL'],
-      Password: process.env['THREESTONE_HEARTH_ADMIN_PASSWORD']
-    })
-      .then((response) => {
-        if (response.status === 200) {
-          callback(null, 'Login was successful.')
-        }
-      })
-      .catch((error) => {
-        // write error handling for wrong e-mail or password here
+// async.series([
+//   (callback) => {
+//     axios.post('https://admin.threestonehearth.com/login', {
+//       Email: process.env['THREESTONE_HEARTH_ADMIN_EMAIL'],
+//       Password: process.env['THREESTONE_HEARTH_ADMIN_PASSWORD']
+//     })
+//       .then((response) => {
+//         if (response.status === 200) {
+//           callback(null, 'Login was successful.')
+//         }
+//       })
+//       .catch((error) => {
+//         // write error handling for wrong e-mail or password here
 
-        callback(error, null)
-      })
-  },
-  (callback) => {
-    axios.get('https://admin.threestonehearth.com/Admin/Product/BulkEdit')
-      .then((response) => {
-        if (response.status === 200) {
-          const html = response.data
-          console.log(html)
-          callback(null, html)
-        }
-      })
-      .catch((error) => {
-        callback(error, null)
-      })
-  }
-], (error, results) => {
-  console.log(results)
-})
+//         callback(error, null)
+//       })
+//   },
+//   (callback) => {
+//     axios.get('https://admin.threestonehearth.com/Admin/Product/BulkEdit')
+//       .then((response) => {
+//         if (response.status === 200) {
+//           const html = response.data
+//           console.log(html)
+//           callback(null, html)
+//         }
+//       })
+//       .catch((error) => {
+//         callback(error, null)
+//       })
+//   }
+// ], (error, results) => {
+//   console.log(results)
+// })
 
   // WEDNESDAY FLOW
   // clicking on the yellow 'ADMINISTRATION' banner after logging in: https://admin.threestonehearth.com/admin/customer
