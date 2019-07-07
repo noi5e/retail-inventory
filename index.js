38b52e015769cd14781b07e0798cd6fa1a5a234f
Will Gutierrez <will.gutierrez@gmail.com>
2019-07-07 00:34:35 -0700

    opens csv file, and converts to JSON

--- index.js
+++ index.js
@@ -1,28 +1,33 @@
 require('dotenv').config()
-// const axios = require('axios')
-// const cheerio = require('cheerio')
-// const fs = require('fs')
-// const async = require('async')
 
+const csvParse = require('csv-parser')
+const fs = require('fs')
 const puppeteer = require('puppeteer')
 
 async function loginToAdminAccount() {
   const browser = await puppeteer.launch()
   const page = await browser.newPage()
-  await page.goto('https://admin.threestonehearth.com/login')
-  await page.type('#Email', process.env['THREESTONE_HEARTH_ADMIN_EMAIL'])
-  await page.type('#Password', process.env['THREESTONE_HEARTH_ADMIN_PASSWORD'])
-  await page.keyboard.press('Enter')
-  await page.waitForNavigation()
-  await page.goto('https://admin.threestonehearth.com/Admin/Product/BulkEdit')
-  await page.waitForSelector('td[role="gridcell"]')
-  await page.screenshot({ path: './screenshots/' + new Date().getTime() + '.png' })
-  browser.close()
+
+  let retailProducts = []
+
+  fs.createReadStream('./csv/online-retail-products.csv')
+    .pipe(csvParse())
+    .on('data', (data) => {
+      retailProducts.push(data)
+    })
+    .on('end', () => {
+      console.log(retailProducts)
+    })
+
+  // await page.goto('https://admin.threestonehearth.com/login')
+  // await page.type('#Email', process.env['THREESTONE_HEARTH_ADMIN_EMAIL'])
+  // await page.type('#Password', process.env['THREESTONE_HEARTH_ADMIN_PASSWORD'])
+  // await page.keyboard.press('Enter')
+  // await page.waitForNavigation()
+  // await page.goto('https://admin.threestonehearth.com/Admin/Product/BulkEdit')
+  // await page.waitForSelector('td[role="gridcell"]')
+  // await page.screenshot({ path: './screenshots/' + new Date().getTime() + '.png' })
+  // browser.close()
 }
 
 loginToAdminAccount()
-
-  // WEDNESDAY FLOW
-  // clicking on the yellow 'ADMINISTRATION' banner after logging in: https://admin.threestonehearth.com/admin/customer
-  // Catalog -> Products -> Product Inventory: https://admin.threestonehearth.com/Admin/Product/BulkEdit
-  // The relevant menu will be selected under "Weekly Menu" 
