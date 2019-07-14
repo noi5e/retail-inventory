ce650cb993282edc2ad2e45c6380122adf0c28ac
Will Gutierrez <will.gutierrez@gmail.com>
2019-07-13 17:19:38 -0700

    finds data-uid of a given <tr> element

--- index.js
+++ index.js
@@ -9,8 +9,6 @@ async function loginToAdminAccount() {
   const browser = await puppeteer.launch()
   const page = await browser.newPage()
 
-  let retailProducts = []
-
   console.log('Loading admin homepage...')
   await page.goto('https://admin.threestonehearth.com/login')
   console.log('Entering admin e-mail...')
@@ -23,13 +21,14 @@ async function loginToAdminAccount() {
   await page.waitForNavigation()
   console.log('Going to Product Inventory page...')
   await page.goto('https://admin.threestonehearth.com/Admin/Product/BulkEdit')
-  console.log('Waiting until inventory loads...')
+  console.log('Waiting for inventory to load...')
   await page.waitForSelector('td[role="gridcell"]')
 
-
+  console.log('Opening and reading CSV into parser...')
   const readStream = fs.createReadStream('./csv/online-retail-products.csv')
-
   const csvParsing = readStream.pipe(csvParse())
+
+  let retailProducts = []
 
   const fullyParsedStream = new Promise((resolve, reject) => {
     csvParsing.on('data', (data) => retailProducts.push(data))
@@ -37,20 +36,47 @@ async function loginToAdminAccount() {
     readStream.on('error', reject)
   })
 
-  console.log('Streaming CSV into parser...')
   let completedRetailProducts = await fullyParsedStream
 
   let wednesdayPerishables = completedRetailProducts.filter((product) => {
     return product['Weekly Recurring Order'] === "TRUE";
+  }).map((product) => {
+    return { name: product['Exact Name: NOP'] }
   })
+
+  for (let i = 0; i < wednesdayPerishables.length; i++) {
+    const elementHandles = await page.$x(`//tr[td[contains(text(), '${wednesdayPerishables[i].name}')]]`)
+    const secondElementHandle = elementHandles[1]
+    let dataUID = await page.evaluate(() => secondElementHandle.getAttribute('data-uid'))
+    wednesdayPerishables[i].dataId = dataUID
+  }
 
   console.log(wednesdayPerishables)
 
-  // console.log('Iterating through Wednesday perishables')
-  // wednesdayPerishables.forEach((product) => {
-  //   console.log(product['Exact Name: NOP'])
+  //  <tr class="k-alt" data-uid="someString" role="row">
+  //    <td style="vertical-align:top" role="gridcell">Recurring</td>
+  //    <td>Beber Chocolate Almond Milk</td>
+  //    <td><img src="published-checkmark.gif" /></td>
+  //    <td>$Price</td>
+  //    <td>Blank Cell</td>
+  //    <td>Inventory #</td>
+  //    <td><img src="another-checkmark.gif" /></td>
+  //    <td>To Sell #</td>
+  //    <td><a>Sold</a></td>
+  //    <td>Remaining To Cell</td>
+  //    <td><a>View Product</a></td>
+  //  </tr>
 
-  // })
+  // //tr/td[contains(text() ,'string in quotes')]
+
+  // const elementHandles = await page.$x(`//tr[td[contains(text(), 'Beber Chocolate Almond Milk')]]`)
+  const elementHandles = await page.$x(`//tr[td[contains(text(), '${wednesdayPerishables[0].name}')]]`)
+  const roleHandle = await elementHandles[1].getProperty('outerHTML')
+  const roleJSON = await roleHandle.jsonValue()
+  console.log(roleJSON)
+
+  let role = await page.evaluate(element => element.getAttribute('data-uid'), elementHandles[1])
+  console.log(role)
 
   // await page.screenshot({ path: './screenshots/' + new Date().getTime() + '.png' })
   browser.close()
