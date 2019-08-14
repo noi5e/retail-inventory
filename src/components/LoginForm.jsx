87fde827b1ee7fe3bef4318aabeea0310bb16f3d
Will Gutierrez <will.gutierrez@gmail.com>
2019-08-13 20:21:05 -0700

    add Material-UI Button component

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,31 +1,66 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { makeStyles } from '@material-ui/core/styles'
-import Input from '@material-ui/core/Input'
+import TextField from '@material-ui/core/TextField'
+import Button from '@material-ui/core/Button'
 
-const useStyles = makeStyles(theme => ({
-  container: {
-    display: 'flex',
-    flexWrap: 'wrap'
-  },
-  input: {
-    margin: theme.spacing(1)
+function LoginForm () {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    console.log('handle submit triggered')
+    // if (!email || !password) return
+    // console.log(email + ', ' + password)
+    // setEmail('')
+    // setPassword('')
   }
-}));
 
-class LoginForm extends React.Component {
-  render() {
-    const classes = useStyles();
+  const useStyles = makeStyles(theme => ({
+    container: {
+      display: 'flex',
+      flexWrap: 'wrap'
+    },
+    TextField: {
+      margin: theme.spacing(1)
+    }
+  }))
 
-    return (
-      <Input
-        placeholder="Username"
-        className={classes.input}
+  const classes = useStyles()
+
+  return (
+    <form className={classes.container} onSubmit={handleSubmit} noValidate>
+      <TextField
+        id="email"
+        placeholder="NOP Email"
+        className={classes.TextField}
         inputProps={{
-          'aria-label': 'username'
+          'aria-label': 'email'
         }}
+        required
+        autoFocus
+        value={email}
+        onChange={e => setEmail(e.target.value)}
       />
-    );
-  }
+      <TextField
+        id="password"
+        placeholder="Password"
+        className={classes.TextField}
+        inputProps={{
+          'aria-label': 'password'
+        }}
+        required
+        type="password"
+        value={password}
+        onChange={e => setPassword(e.target.value)}
+      />
+      <Button
+        type="submit"
+      >
+        Sign In
+      </Button>
+    </form>
+  )
 }
 
-export default LoginForm;
+export default LoginForm
